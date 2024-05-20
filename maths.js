a4cef993442cd397cef36e2312be7cb5ed40554d
Rohan Badiger <80166865+badigerohan@users.noreply.github.com>
2024-05-20 22:23:55 +0530

    maths.js

--- maths.js
+++ maths.js
@@ -1,7 +1,5 @@
 var answer;
 var score = 0;
-// var backgroundImages = [];
-// var backgroundImages1 = [];
 
 function nextQuestion(){
     const n1 = Math.floor(Math.random()*5);
@@ -20,15 +18,11 @@ function checkAnswer() {
         score++;
         console.log(`Correct!!. Score =${score}`);
         alert('Well Done!! Correct Answer');
-        // backgroundImages.push(`url('images/background3.svg')`);
-        // document.body.style.backgroundImage = backgroundImages;
     }
     if(prediction != answer){
             score--;
             console.log(`Wrong!!. Score =${score}`);
-            alert('Oops!! Incorrect Answer');
-            // backgroundImages1.push(`url('images/wrong_1.svg')`);
-            // document.body.style.backgroundImage = backgroundImages1;   
+            alert('Oops!! Incorrect Answer'); 
     }
 }
 
