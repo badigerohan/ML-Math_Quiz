7c3e737be28155e8996740fd2e66bcf88a6be8b8
Rohan Badiger <80166865+badigerohan@users.noreply.github.com>
2024-05-20 22:25:19 +0530

    processing.js

--- processing.js
+++ processing.js
@@ -5,18 +5,14 @@ async function loadModel() {
 }
 
 function predictImage() {
-            // console.log('processing.....');
-      
       let image = cv.imread(canvas);
       // converting to grayscale
       cv.cvtColor(image, image, cv.COLOR_RGBA2GRAY, 0);
       // converting grey to black & white
       cv.threshold(image, image, 175, 255, cv.THRESH_BINARY);
-      // contours (getting outlines)
       let contours = new cv.MatVector();
       let hierarchy = new cv.Mat();
       cv.findContours(image, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
-      // extracting the necessary region of interest(roi)
       let cnt = contours.get(0);
       let rect = cv.boundingRect(cnt);
       // cropping the roi
@@ -39,26 +35,21 @@ function predictImage() {
       let newSize = new cv.Size(width, height);
       cv.resize(image, image, newSize, 0, 0, cv.INTER_AREA); 
 
-      // Adding Padding to the image
       const LEFT = Math.ceil(4 + (20-width)/2);
       const RIGHT = Math.floor(4 + (20-width)/2);
       const TOP = Math.ceil(4 + (20-height)/2);
       const BOTTOM = Math.floor(4 + (20-height)/2);
-            // console.log(`top=${TOP},bottom=${BOTTOM},left=${LEFT},right=${RIGHT}`);
       
       const BLACK = new cv.Scalar(0, 0, 0, 0);
       cv.copyMakeBorder(image, image, TOP, BOTTOM, LEFT, RIGHT, cv.BORDER_CONSTANT, BLACK);
 
-      // center of mass
       cv.findContours(image, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
       cnt = contours.get(0);
       const Moments = cv.moments(cnt, false);
 
-      const cx = Moments.m10/Moments.m00 // m00 -area of image
-      const cy = Moments.m01/Moments.m00 // m10- mass on x axis; m01- mass on y axis
-            // console.log(`M00=${Moments.m00}, cx=${cx}, cy=${cy}`);
+      const cx = Moments.m10/Moments.m00 
+      const cy = Moments.m01/Moments.m00 
 
-      // shifting the image to centre of mass
       const X_SHIFT = Math.round(image.cols/2.0 - cx);
       const Y_SHIFT = Math.round(image.rows/2.0 - cy);
 
@@ -66,36 +57,23 @@ function predictImage() {
       const M = cv.matFromArray(2,3,cv.CV_64FC1, [1,0,X_SHIFT, 0,1,Y_SHIFT]);
       cv.warpAffine(image, image, M, newSize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, BLACK);
       
-      // normalizing the pixels (pixel / 255)
+
       let pixelValues = image.data;
-            // console.log(`pixels=${pixelValues}`);
-            // converting to float
       pixelValues = Float32Array.from(pixelValues);
 
       pixelValues = pixelValues.map(function (item) {
             return item/255.0;
       });
-            // console.log(`scaled values=${pixelValues}`);
 
       //Create tensor
       const X = tf.tensor([pixelValues]);
-            // console.log(`Shape of tensor= ${X.shape}`);
-            // console.log(`dtype= ${X.dtype}`);
-
       // predicting
       const result = model.predict(X);
       result.print();
-            // console.log(tf.memory());
       
       // storing the output
       const output = result.dataSync()[0];
 
-            // // Testing 
-            // const outputCanvas = document.createElement('CANVAS');
-            // cv.imshow(outputCanvas, image);
-            // document.body.appendChild(outputCanvas);
-      
-      // Cleaning
       image.delete();
       contours.delete();
       cnt.delete();
